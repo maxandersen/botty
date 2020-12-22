7845af4a3bdb1926f2f7dd6fd81190ff1c232ccb
Max Rydahl Andersen <manderse@redhat.com>
2020-12-22 23:15:49 +0100

    test

--- bot.java
+++ bot.java
@@ -3,6 +3,7 @@
 //DEPS io.smallrye.reactive:mutiny:0.11.0
 //DEPS io.quarkiverse.githubapp:quarkiverse-github-app:0.0.1
 //FILES application.properties
+package booty;
 
 import static java.lang.System.*;
 
@@ -12,6 +13,9 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
 import org.kohsuke.github.GHEventPayload;
 
 import io.quarkiverse.githubapp.event.Issue;
@@ -20,11 +24,15 @@ import io.quarkus.runtime.StartupEvent;
 import org.jboss.logging.Logger;
 
 
-public class bot {
+@QuarkusMain
+public class bot implements QuarkusApplication {
 
   private static final Logger LOG = Logger.getLogger(bot.class);
 
-    
+  public static void main(String[] args) {
+    Quarkus.run(bot.class, args);
+  }
+
     void init(@Observes StartupEvent startupEvent) {
             LOG.info("››› xybot running " + this);
     }
@@ -32,4 +40,10 @@ public class bot {
     void onOpen(@Issue.Opened GHEventPayload.Issue issuePayload) throws IOException {
   		issuePayload.getIssue().comment("Hello from MyGitHubApp");
     }
+
+  @Override
+  public int run(String... args) throws Exception {
+    Quarkus.waitForExit();
+    return 0;
+  }
 }
