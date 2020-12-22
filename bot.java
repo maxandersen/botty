0ddb4f54bf2e9a4e57805fade3a504ea1f9edaf1
Max Rydahl Andersen <manderse@redhat.com>
2020-12-23 00:16:57 +0100

    add readme.adoc

--- bot.java
+++ bot.java
@@ -1,26 +1,14 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-// //DEPS <dependency1> <dependency2>
 //DEPS io.smallrye.reactive:mutiny:0.11.0
 //DEPS io.quarkiverse.githubapp:quarkiverse-github-app:0.0.1
-//DEPS io.quarkus:quarkus-bootstrap-gradle-resolver:1.10.3.Final
 //FILES application.properties
 
-import static java.lang.System.*;
-
 import java.io.IOException;
+import java.lang.System.Logger;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import javax.inject.Inject;
-
-import io.quarkus.runtime.Quarkus;
-import io.quarkus.runtime.QuarkusApplication;
-import io.quarkus.runtime.annotations.QuarkusMain;
 import org.kohsuke.github.GHEventPayload;
 
 import io.quarkiverse.githubapp.event.Issue;
-import io.quarkus.runtime.StartupEvent;
-
 import org.jboss.logging.Logger;
 
 
